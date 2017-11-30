f0983a62e0ea0a796cbf087b7b8c009a7f0fa579
Chris Connor <chris@chrisconnor.co.uk>
2017-11-30 13:03:19 +0000

    Test area created for working on functionality

---
+++ testground/test.c
@@ -0,0 +1,26 @@
+// Cwk2: client.c - message length headers with variable sized payloads
+//  also use of readn() and writen() implemented in separate code module
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#include "rdwrn.h"
+
+#define INPUTSIZ 10
+
+
+int main(void)
+{
+
+
+	// TESTING GROUND FOR FUNCTIONS
+
+    exit(EXIT_SUCCESS);
+} // end main()
